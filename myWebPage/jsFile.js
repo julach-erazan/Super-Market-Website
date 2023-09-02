1a9f9017d2eadbf1fb65f3ca5c1093e64a7a5644
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2023-09-02 11:24:41 +0530

    update

---
+++ myWebPage/jsFile.js
@@ -0,0 +1,335 @@
+document.getElementById("index0").addEventListener("click",function(){
+			document.querySelector(".container").style.opacity="0.4";
+			document.getElementById("index0").disabled=true;
+			document.querySelector(".sub-container").style.display="flex";
+			document.querySelector(".popup-box0").style.display="flex";
+			})
+document.querySelector(".closebtn0").addEventListener("click",function(){
+			document.querySelector(".popup-box0").style.display="none";
+			document.querySelector(".sub-container").style.display="none";
+			document.querySelector(".container").style.opacity="1";
+			document.getElementById("index0").disabled=false;
+			})
+			
+document.getElementById("index1").addEventListener("click",function(){
+			document.querySelector(".container").style.opacity="0.4";
+			document.getElementById("index1").disabled=true;
+			document.querySelector(".sub-container").style.display="flex";
+			document.querySelector(".popup-box1").style.display="flex";
+		})
+		document.querySelector(".closebtn1").addEventListener("click",function(){
+			document.querySelector(".popup-box1").style.display="none";
+			document.querySelector(".sub-container").style.display="none";
+			document.querySelector(".container").style.opacity="1";
+			document.getElementById("index1").disabled=false;
+		})
+		
+		
+		document.getElementById("index2").addEventListener("click",function(){
+			document.querySelector(".container").style.opacity="0.4";
+			document.getElementById("index2").disabled=true;
+			document.querySelector(".sub-container").style.display="flex";
+			document.querySelector(".popup-box2").style.display="flex";
+		})
+		document.querySelector(".closebtn2").addEventListener("click",function(){
+			document.querySelector(".popup-box2").style.display="none";
+			document.querySelector(".sub-container").style.display="none";
+			document.querySelector(".container").style.opacity="1";
+			document.getElementById("index2").disabled=false;
+		})
+		
+		
+		document.getElementById("index3").addEventListener("click",function(){
+			document.querySelector(".container").style.opacity="0.4";
+			document.getElementById("index3").disabled=true;
+			document.querySelector(".sub-container").style.display="flex";
+			document.querySelector(".popup-box3").style.display="flex";
+		})
+		document.querySelector(".closebtn3").addEventListener("click",function(){
+			document.querySelector(".popup-box3").style.display="none";
+			document.querySelector(".sub-container").style.display="none";
+			document.querySelector(".container").style.opacity="1";
+			document.getElementById("index3").disabled=false;
+		})
+		
+		
+		
+		document.getElementById("index4").addEventListener("click",function(){
+			document.querySelector(".container").style.opacity="0.4";
+			document.getElementById("index4").disabled=true;
+			document.querySelector(".sub-container").style.display="flex";
+			document.querySelector(".popup-box4").style.display="flex";
+		})
+		document.querySelector(".closebtn4").addEventListener("click",function(){
+			document.querySelector(".popup-box4").style.display="none";
+			document.querySelector(".sub-container").style.display="none";
+			document.querySelector(".container").style.opacity="1";
+			document.getElementById("index4").disabled=false;
+		})
+		
+		
+		document.getElementById("index5").addEventListener("click",function(){
+			document.querySelector(".container").style.opacity="0.4";
+			document.getElementById("index5").disabled=true;
+			document.querySelector(".sub-container").style.display="flex";
+			document.querySelector(".popup-box5").style.display="flex";
+		})
+		document.querySelector(".closebtn5").addEventListener("click",function(){
+			document.querySelector(".popup-box5").style.display="none";
+			document.querySelector(".sub-container").style.display="none";
+			document.querySelector(".container").style.opacity="1";
+			document.getElementById("index5").disabled=false;
+		})
+		
+		
+		document.getElementById("index6").addEventListener("click",function(){
+			document.querySelector(".container").style.opacity="0.4";
+			document.getElementById("index6").disabled=true;
+			document.querySelector(".sub-container").style.display="flex";
+			document.querySelector(".popup-box6").style.display="flex";
+		})
+		document.querySelector(".closebtn6").addEventListener("click",function(){
+			document.querySelector(".popup-box6").style.display="none";
+			document.querySelector(".sub-container").style.display="none";
+			document.querySelector(".container").style.opacity="1";
+			document.getElementById("index6").disabled=false;
+		})
+		
+		
+		document.getElementById("index7").addEventListener("click",function(){
+			document.querySelector(".container").style.opacity="0.4";
+			document.getElementById("index7").disabled=true;
+			document.querySelector(".sub-container").style.display="flex";
+			document.querySelector(".popup-box7").style.display="flex";
+		})
+		document.querySelector(".closebtn7").addEventListener("click",function(){
+			document.querySelector(".popup-box7").style.display="none";
+			document.querySelector(".sub-container").style.display="none";
+			document.querySelector(".container").style.opacity="1";
+			document.getElementById("index7").disabled=false;
+		})
+		
+		
+		document.getElementById("index8").addEventListener("click",function(){
+			document.querySelector(".container").style.opacity="0.4";
+			document.getElementById("index8").disabled=true;
+			document.querySelector(".sub-container").style.display="flex";
+			document.querySelector(".popup-box8").style.display="flex";
+		})
+		document.querySelector(".closebtn8").addEventListener("click",function(){
+			document.querySelector(".popup-box8").style.display="none";
+			document.querySelector(".sub-container").style.display="none";
+			document.querySelector(".container").style.opacity="1";
+			document.getElementById("index8").disabled=false;
+		})
+		
+var num1,num2,num3,num4,num5,num6,num7,num8,num9,sum1,sum2,sum3,sum4,sum5,sum6,sum7,sum8,sum9;
+var data=0;
+
+document.getElementById("counting1").innerText = data;
+document.getElementById("counting2").innerText = data;
+document.getElementById("counting3").innerText = data;
+document.getElementById("counting4").innerText = data;
+document.getElementById("counting5").innerText = data;
+document.getElementById("counting6").innerText = data;
+document.getElementById("counting7").innerText = data;
+document.getElementById("counting8").innerText = data;
+document.getElementById("counting9").innerText = data;
+			
+	function sumValue1(){
+				
+		num1 = 1096;
+				
+		sum1 = num1*data;
+		document.formCalc1.textAmount1.value = sum1;
+	}
+	
+	function sumValue2(){
+				
+		num2 = 405;
+				
+		sum2 = num2*data;
+		document.formCalc2.textAmount2.value = sum2;
+	}
+	
+	function sumValue3(){
+				
+		num3 = 455;
+				
+		sum3 = num3*data;
+		document.formCalc3.textAmount3.value = sum3;
+	}
+	
+	function sumValue4(){
+				
+		num4 = 240;
+				
+		sum4 = num4*data;
+		document.formCalc4.textAmount4.value = sum4;
+	}
+	
+	function sumValue5(){
+				
+		num5 = 496;
+				
+		sum5 = num5*data;
+		document.formCalc5.textAmount5.value = sum5;
+	}
+	
+	function sumValue6(){
+				
+		num6 = 85;
+				
+		sum6 = num6*data;
+		document.formCalc6.textAmount6.value = sum6;
+	}
+	
+	function sumValue7(){
+				
+		num7 = 2485;
+				
+		sum7 = num7*data;
+		document.formCalc7.textAmount7.value = sum7;
+	}
+	
+	function sumValue8(){
+				
+		num8 = 585;
+				
+		sum8 = num8*data;
+		document.formCalc8.textAmount8.value = sum8;
+	}
+	
+	function sumValue9(){
+				
+		num9 = 130;
+				
+		sum9 = num9*data;
+		document.formCalc9.textAmount9.value = sum9;
+	}
+			
+	function increment1(){
+		if(data<10){
+			data = data+1;
+			document.getElementById("counting1").innerText = data;
+		}
+	}
+
+	function decrement1(){
+		if(data>0){
+			data = data-1;
+			document.getElementById("counting1").innerText = data;
+		}
+	}
+	
+	function increment2(){
+		if(data<10){
+			data = data+1;
+			document.getElementById("counting2").innerText = data;
+		}
+	}
+
+	function decrement2(){
+		if(data>0){
+			data = data-1;
+			document.getElementById("counting2").innerText = data;
+		}
+	}
+	
+	function increment3(){
+		if(data<10){
+			data = data+1;
+			document.getElementById("counting3").innerText = data;
+		}
+	}
+
+	function decrement3(){
+		if(data>0){
+			data = data-1;
+			document.getElementById("counting3").innerText = data;
+		}
+	}
+	
+	function increment4(){
+		if(data<10){
+			data = data+1;
+			document.getElementById("counting4").innerText = data;
+		}
+	}
+
+	function decrement4(){
+		if(data>0){
+			data = data-1;
+			document.getElementById("counting4").innerText = data;
+		}
+	}
+	
+	function increment5(){
+		if(data<10){
+			data = data+1;
+			document.getElementById("counting5").innerText = data;
+		}
+	}
+
+	function decrement5(){
+		if(data>0){
+			data = data-1;
+			document.getElementById("counting5").innerText = data;
+		}
+	}
+	
+	function increment6(){
+		if(data<10){
+			data = data+1;
+			document.getElementById("counting6").innerText = data;
+		}
+	}
+
+	function decrement6(){
+		if(data>0){
+			data = data-1;
+			document.getElementById("counting6").innerText = data;
+		}
+	}
+	
+	function increment7(){
+		if(data<10){
+			data = data+1;
+			document.getElementById("counting7").innerText = data;
+		}
+	}
+
+	function decrement7(){
+		if(data>0){
+			data = data-1;
+			document.getElementById("counting7").innerText = data;
+		}
+	}
+	
+	function increment8(){
+		if(data<10){
+			data = data+1;
+			document.getElementById("counting8").innerText = data;
+		}
+	}
+
+	function decrement8(){
+		if(data>0){
+			data = data-1;
+			document.getElementById("counting8").innerText = data;
+		}
+	}
+	
+	function increment9(){
+		if(data<10){
+			data = data+1;
+			document.getElementById("counting9").innerText = data;
+		}
+	}
+
+	function decrement9(){
+		if(data>0){
+			data = data-1;
+			document.getElementById("counting9").innerText = data;
+		}
+	}
